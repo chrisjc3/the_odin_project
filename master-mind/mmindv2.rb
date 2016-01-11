865bfe175dddf79d271d979702eb93a7b9da34a6
Chris J Crichton <chrisjcrichton@hotmail.com>
2016-01-11 15:41:34 -0500

    first ai attempt (easy to beat/break)

--- master-mind/mmindv2.rb
+++ master-mind/mmindv2.rb
@@ -123,18 +123,212 @@ def return_feedback(a, b)
 	return feedback
 end
 
+def nilbyside(x, arr)
+	if x == 1 
+		if arr[0] == nil 
+			return 0
+		elsif arr[1] == nil
+			return 1
+		else
+			return nil
+		end
+	end
+	if x == 2
+		if arr[2] == nil 
+			return 2
+		elsif arr[3] == nil
+			return 3
+		else
+			return nil
+		end
+	end
+end
+
+########################CRAFTED ANSWERS 6:8 START######################
+def craft6
+	ai_guess = [nil, nil, nil, nil]
+
+###########WON'T FILL GUESS IF 2+ COLORS IN ANSWER###################
+###########2 GREENS FUCKS IT ALL UP##########
+##########START A V3.....###########
+	if $fb1.include? "correct"
+		#assume first col in top half, change at fb3 if found to not be true
+		ai_guess[0] = $col_arr[0]
+		if $fb2.count(nil) < 3
+			nf1 = nilbyside(1, ai_guess)
+			nf2 = nilbyside(2, ai_guess)	
+			if $fb2.count("correct") == 2
+				ai_guess[nf2] = $col_arr[1]
+			end
+			if $fb2.count("present") == 2
+				ai_guess[nf2] = $col_arr[1]
+			end
+			if $fb2.count("present") == 1
+				ai_guess[nf2] = $col_arr[1]
+			end
+		end
+
+		if $fb3.count(nil) < 4
+			nf1 = nilbyside(1, ai_guess)
+			nf2 = nilbyside(2, ai_guess)	
+			if $fb3.count("correct") == 2
+				ai_guess[nf2] = $col_arr[2]
+			end
+			if $fb3.count("present") == 2
+				if nf1 != nil
+					ai_guess[nf1] = $col_arr[2]
+				elsif nf2 != nil
+					ai_guess[nf2] = $col_arr[2]
+				end
+			end
+			if $fb3.count("present") == 1
+				ai_guess[nf2] = $col_arr[0]
+				ai_guess[0] = nil
+			end
+		end
+
+		if $fb4.count(nil) < 3
+			nf1 = nilbyside(1, ai_guess)
+			nf2 = nilbyside(2, ai_guess)	
+			if $fb4.count("correct") == 2
+				ai_guess[nf2] = $col_arr[3]
+			end
+			if $fb4.count("present") == 2
+				ai_guess[nf1] = $col_arr[3]
+			end
+			if $fb4.count("present") == 1
+				if nf1 != nil
+					ai_guess[nf1] = $col_arr[3]
+				elsif nf2 != nil
+					ai_guess[nf2] = $col_arr[3]
+				end
+			end
+		end
+
+		if $fb5.count(nil) < 3
+			nf1 = nilbyside(1, ai_guess)
+			nf2 = nilbyside(2, ai_guess)	
+			if $fb5.count("correct") == 2
+				ai_guess[nf2] = $col_arr[4]
+			end
+			if $fb5.count("present") == 2
+				ai_guess[nf1] = $col_arr[4]
+			end
+			if $fb5.count("present") == 1
+				if nf1 != nil
+					ai_guess[nf1] = $col_arr[4]
+				elsif nf2 != nil
+					ai_guess[nf2] = $col_arr[4]
+				end
+			end
+		end
+
+	end
+
+###########WORK ON IF 0's for first (this confirms col_arr5 exists)###################
+
+	if $fb1.count("correct") == 0 and $fb2.count(nil) < 4
+		nf1 = nilbyside(1, ai_guess)
+		nf2 = nilbyside(2, ai_guess)	
+		if $fb2.include? "correct"
+			ai_guess[nf2] = $col_arr[1]
+		end
+		if $fb2.include? "present" 
+			ai_guess[nf1] = $col_arr[1]
+		end
+	elsif $fb1.count("correct") == 0 and $fb3.count(nil) < 4
+		nf1 = nilbyside(1, ai_guess)
+		nf2 = nilbyside(2, ai_guess)	
+		if $fb3.include? "correct"
+			ai_guess[nf2] = $col_arr[2]
+		end
+		if $fb3.include? "present" 
+			ai_guess[nf1] = $col_arr[2]
+		end
+	elsif $fb1.count("correct") == 0 and $fb4.count(nil) < 4
+		nf1 = nilbyside(1, ai_guess)
+		nf2 = nilbyside(2, ai_guess)	
+		if $fb4.include? "correct"
+			ai_guess[nf2] = $col_arr[3]
+		end
+		if $fb4.include? "present" 
+			ai_guess[nf1] = $col_arr[3]
+		end
+	elsif $fb1.count("correct") == 0 and $fb5.count(nil) < 4
+		nf1 = nilbyside(1, ai_guess)
+		nf2 = nilbyside(2, ai_guess)	
+		if $fb5.include? "correct"
+			ai_guess[nf2] = $col_arr[4]
+		end
+		if $fb5.include? "present" 
+			ai_guess[nf1] = $col_arr[4]
+		end
+	end
+
+	if ai_guess.count(nil) > 0 
+		nf1 = nilbyside(1, ai_guess)
+		nf2 = nilbyside(2, ai_guess)	
+		if nf1 != nil 
+			ai_guess[nf1] = $col_arr[5]
+		elsif nf2 != nil
+			ai_guess[nf2] = $col_arr[5]
+		end
+	end
+
+###########WON'T FILL GUESS IF 2+ COLORS IN ANSWER###################
+
+	return ai_guess
+end
+
+def craft7 
+	#assumes craft6 lands 4 present (it should)
+	ai_guess = [nil, nil, nil, nil]
+	if $fb6.count("present") == 4
+		#flip all in halves if 4 are wrong
+		ai_guess[0] = $ag6[1]
+		ai_guess[1] = $ag6[0]
+		ai_guess[2] = $ag6[3]
+		ai_guess[3] = $ag6[2]
+	end
+	if $fb6.count("present") == 2
+		#flip first 2 if 2 are wrong
+		ai_guess[0] = $ag6[0]
+		ai_guess[1] = $ag6[1]
+		ai_guess[2] = $ag6[3]
+		ai_guess[3] = $ag6[2]
+	end
+	return ai_guess
+end
+
+def craft8
+	ai_guess = [nil, nil, nil, nil]
+	if $fb6.count("present") == 2
+		#flip first 2 if 2 are wrong
+		ai_guess[0] = $ag6[1]
+		ai_guess[1] = $ag6[0]
+		ai_guess[2] = $ag6[2]
+		ai_guess[3] = $ag6[3]
+	end
+	return ai_guess
+end
+
+
+
+########################CRAFTED ANSWERS 6:8 END######################
+
+
 ################GAME START######################
 
-col_arr = ["green","red","blue","black","purple","yellow"]
+$col_arr = ["green","red","blue","black","purple","yellow"]
 
 puts "Who picks colors? 0 = ai ; 1 = you"
 ply = gets.chomp
 ply = ply.to_i
 ########################HUMAN##########################
 if ply == 0 
-	answer = ai_col_tgt(col_arr)
+	answer = ai_col_tgt($col_arr)
 	puts "Answer: " + answer.to_s
-	puts "Please choose from: " + col_arr.to_s
+	puts "Please choose from: " + $col_arr.to_s
 
 	i = 1
 	until i == 8
@@ -157,187 +351,122 @@ if ply == 0
 
 ########################AI##########################
 elsif ply == 1
-	puts "Please choose from: " + col_arr.to_s
+	puts "Please choose from: " + $col_arr.to_s
 	answer = guess_tgt
 	ai_guess = []
 	i = 0
 
 
-	until i == 7
+	until i == 8
 ###############FIRST 5 GUESSES ARE PREDETERMINED###########
 		#Guess 1
 		if i == 0
-			ag = [col_arr[0].to_s,col_arr[0].to_s,col_arr[0].to_s,col_arr[0].to_s]
+			ag = [$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[0].to_s]
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb1 = return_feedback(answer, ag)
+			$fb1 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb1.to_s
+			puts "Feedback Number " + fno.to_s + ": " + $fb1.to_s
 		end
 		#Guess 2
 		if i == 1
-			ag = [col_arr[0].to_s,col_arr[0].to_s,col_arr[i].to_s,col_arr[i].to_s]
+			ag = [$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[i].to_s,$col_arr[i].to_s]
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb2 = return_feedback(answer, ag)
+			$fb2 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb2.to_s
+			puts "Feedback Number " + fno.to_s + ": " + $fb2.to_s
 		end
 		#Guess 3
 		if i == 2
-			ag = [col_arr[0].to_s,col_arr[0].to_s,col_arr[i].to_s,col_arr[i].to_s]
+			ag = [$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[i].to_s,$col_arr[i].to_s]
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb3 = return_feedback(answer, ag)
+			$fb3 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb3.to_s
+			puts "Feedback Number " + fno.to_s + ": " + $fb3.to_s
 		end
 		#Guess 4
 		if i == 3
-			ag = [col_arr[0].to_s,col_arr[0].to_s,col_arr[i].to_s,col_arr[i].to_s]
+			ag = [$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[i].to_s,$col_arr[i].to_s]
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb4 = return_feedback(answer, ag)
+			$fb4 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb4.to_s
+			puts "Feedback Number " + fno.to_s + ": " + $fb4.to_s
 		end
 		#Guess 5
 		if i == 4
-			ag = [col_arr[0].to_s,col_arr[0].to_s,col_arr[i].to_s,col_arr[i].to_s]
+			ag = [$col_arr[0].to_s,$col_arr[0].to_s,$col_arr[i].to_s,$col_arr[i].to_s]
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb5 = return_feedback(answer, ag)
+			$fb5 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb5.to_s
+			puts "Feedback Number " + fno.to_s + ": " + $fb5.to_s
 		end
 ### PRESENCE OF 6 CAN BE ASSUMED BASED ON PRIOR ###
 		#Guess 6
 		#START DIAGNOSING FEEDBACK FOR GUESSES
 		if i == 5
-			tot = 0
-			corrects = fb1.count("correct").to_i
-			presents = fb1.count("present").to_i
-			tot1 = corrects.to_i + presents.to_i
-			if tot1 > 0 
-				j = 0
-				tot = tot1
-				until j == tot1
-					ai_guess.push col_arr[0].to_s
-				j += 1
-				end
+			$ag6 = craft6
+			puts "---------------------------------"
+			puts "Computer guessed: " + $ag6.to_s
+			if answer == $ag6
+				puts "Computer wins!"
+				break
 			end
-			corrects = fb2.count("correct").to_i
-			presents = fb2.count("present").to_i
-			tot2 = corrects.to_i + presents.to_i
-			if tot2 > 0 
-				j = 0
-				tot2 = tot2 - tot
-				until j == tot2
-					ai_guess.push col_arr[1].to_s
-				j += 1
-				end
+			$fb6 = return_feedback(answer, $ag6)
+			fno = i + 1
+			puts "Feedback Number " + fno.to_s + ": " + $fb6.to_s
+		end
+		#Guess 7
+		if i == 6
+			$ag7 = craft7
+			puts "---------------------------------"
+			puts "Computer guessed: " + $ag7.to_s
+			if answer == $ag7
+				puts "Computer wins!"
+				break
 			end
-			corrects = fb3.count("correct").to_i
-			presents = fb3.count("present").to_i
-			tot3 = corrects.to_i + presents.to_i
-			if tot3 > 0 
-				j = 0
-				tot3 = tot3 - tot
-				until j == tot3
-					ai_guess.push col_arr[2].to_s
-				j += 1
-				end
-			end
-			corrects = fb4.count("correct").to_i
-			presents = fb4.count("present").to_i
-			tot4 = corrects.to_i + presents.to_i
-			if tot4 > 0 
-				j = 0
-				tot4 = tot4 - tot
-				until j == tot4
-					ai_guess.push col_arr[3].to_s
-				j += 1
-				end
-			end
-			corrects = fb5.count("correct").to_i
-			presents = fb5.count("present").to_i
-			tot5 = corrects.to_i + presents.to_i
-			if tot > 0 
-				j = 0
-				tot5 = tot5 - tot
-				until j == tot5
-					ai_guess.push col_arr[4].to_s
-				j += 1
-				end
-			end
-
-			if ai_guess.size < 4 
-				until ai_guess.size == 4
-					ai_guess.push col_arr[5].to_s
-				end
-			end
-			ag = ai_guess
+			$fb7 = return_feedback(answer, $ag7)
+			fno = i + 1
+			puts "Feedback Number " + fno.to_s + ": " + $fb7.to_s
+		end
+		#Guess 8
+		if i == 7
+			ag = craft8
 			puts "---------------------------------"
 			puts "Computer guessed: " + ag.to_s
 			if answer == ag
 				puts "Computer wins!"
 				break
 			end
-			fb6 = return_feedback(answer, ag)
+			fb8 = return_feedback(answer, ag)
 			fno = i + 1
-			puts "Feedback Number " + fno.to_s + ": " + fb6.to_s
+			puts "Feedback Number " + fno.to_s + ": " + fb8.to_s
 		end
-		#Guess 7
-
-
-		
-		# if i > 5
-		# afreq = gen_freq(ai_guess)
-		# make_keys(afreq)
-		# 	j = 1
-		# 	rand_guess = []
-		# 	until j == 4
-		# 		x = ai_guess[rand(4)]
-		# 		xmax = find_allow(x)
-		# 		if rand_guess.count(x) < xmax 
-		# 			rand_guess.push ai_guess[rand(4)]
-		# 		end
-		# 		j += 1
-		# 	end
-		# 	ag = rand_guess
-		# 	puts "---------------------------------"
-		# 	puts "Computer guessed: " + ag.to_s
-		# 	if answer == ag
-		# 		puts "Computer wins!"
-		# 		break
-		# 	end
-		# 	fb7 = return_feedback(answer, ag)
-		# 	fno = i + 1
-		# 	puts "Feedback Number " + fno.to_s + ": " + fb7.to_s
-		# end
-		#Guess 8
 	i += 1
-	if i == 7
+	if i == 8
 		puts "Computer lost"
 	end
 	end
@@ -346,3 +475,6 @@ else
 end
 
 ################END######################
+
+
+
